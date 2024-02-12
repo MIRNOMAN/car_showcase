eba4ba225ef55f68c7455f873d35af83dc0ce895
MIRNOMAN <you@example.comabdullahalnoman1509@gmail.com>
2024-02-13 03:03:48 +0600

    first commit

---
+++ components/index.ts
@@ -0,0 +1,11 @@
+import Hero from "./Hero";
+import CustomButton from "./CusmonButton";
+import Navber from "./Navbar";
+import Footer from "./Footer";
+
+export {
+    Hero,
+    CustomButton,
+    Navber,
+    Footer,
+}
